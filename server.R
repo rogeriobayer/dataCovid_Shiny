3799fc1da2589fd9487d03d2be8fbcf87b070c82
Rogério Bayer <tomatebg@gmail.com>
2021-08-18 23:07:44 -0300

    [fix] processamento de dataset e melhorias na ui

--- server.R
+++ server.R
@@ -1,26 +1,17 @@
 load(file = "./dataCovid.RData")
 
-library(datasets)
+library(tidyverse)
+library(ggplot2)
 
-# Define a server for the Shiny app
 function(input, output) {
-    
-    # Fill in the spot we created for a plot
-    output$phonePlot <- renderPlot({
-        
-        library(tidyverse)
-        library(ggplot2)
-        
+    output$graphicPlot <- renderPlot({
+       if(input$switchSelection){
         agr <- aggregate(dataCovid[,input$option], by=list(y=dataCovid[,input$option2]), FUN=sum)
-        library(ggplot2)
-        
-        data <- data.frame(
-            TipoDeTransmissao=dataCovid[,input$option],  
-            CasosNosUltimos7Dias=dataCovid[,input$option2]
-        )
-        
         ggplot(data, aes(x=dataCovid[,input$option], y=dataCovid[,input$option2])) +  geom_bar(stat = "identity")+ xlab(input$option) + ylab(input$option2)
-        # Render a barplot
-    
-    })
+        } else { 
+                barplot(dataCovid[,input$option], 
+                main=input$option,
+                ylab=input$option,
+                xlab=input$option2)
+    }})
 }
--- ui.R
+++ ui.R
@@ -1,25 +1,30 @@
 load(file = "./dataCovid.RData")
 
 library(datasets)
+library(shinyWidgets)
 
 fluidPage(    
     
-    titlePanel("covid19 por transmissao"),
+    titlePanel("Shiny Covid19"),
     
     sidebarLayout(      
         
         sidebarPanel(
-            selectInput("option", "Region:", 
+            selectInput("option", "Eixo X da Tabela:", 
                         choices=colnames(dataCovid)),
-            selectInput("option2", "Region:", 
+            selectInput("option2", "Eixo Y da Tabela:", 
                         choices=colnames(dataCovid)),
             
+            helpText("Escolha a biblioteca de graficos"),
+            
+            switchInput(inputId = "switchSelection", label = "graficos", onLabel= "ggplot", offLabel="barplot" ),
+            
             hr(),
-            helpText("Data from WHO")
+            helpText("Dados provenientes da OMS"),
         ),
         
         mainPanel(
-            plotOutput("phonePlot")  
+            plotOutput("graphicPlot")  
         )
         
     )
